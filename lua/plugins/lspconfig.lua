0c7e94a9e9657763361b89fba853e2c3a60c4a8c
Julian Modanese <julian.modanese@gmail.com>
2026-07-19 10:26:39 +0200

    cleanup

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -176,7 +176,7 @@ local ensure_installed = vim.tbl_keys(servers or {})
 vim.list_extend(ensure_installed, {
   -- You can add other tools here that you want Mason to install
 })
--- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
 require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
 for name, server in pairs(servers) do
